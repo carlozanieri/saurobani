1cb9c8b547b44ff1007a33ff5d16cfabbc13cce3
carlozanieri <carlo.zanieri@gmail.com>
2023-02-21 13:58:45 +0100

    Versione 1.5

--- hello.py
+++ hello.py
@@ -95,7 +95,7 @@ class HelloWorld():
         
         #luogo = request.params[0]
 
-        return tmp.render( slider=Connect.slider("", luogo), renderer="json")
+        return tmp.render( slider=Connect.sliderc("", luogo), renderer="json")
 
 
     @cherrypy.expose
